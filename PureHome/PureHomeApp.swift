ac868d04274b0509a342356b3bef5da2bd0e1ff3
kartikayy007 <kartikayy6969@gmail.com>
2026-02-20 14:30:00 +0530

    Initial commit

---
+++ PureHome/PureHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureHomeApp.swift
+//  PureHome
+//
+//  Created by kartikay on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PureHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
